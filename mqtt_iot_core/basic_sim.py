fd6691a544bf1289423626a4268d843d61b5b69b
Priyesh_Rathore <71845834+gp7ruthless@users.noreply.github.com>
2022-08-03 13:11:02 +0530

    Create basic_sim.py

---
+++ mqtt_iot_core/basic_sim.py
@@ -0,0 +1,60 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+from awscrt import io, mqtt, auth, http
+from awsiot import mqtt_connection_builder
+import time
+import json
+import datetime
+import random
+
+# Define ENDPOINT, CLIENT_ID, PATH_TO_CERTIFICATE, PATH_TO_PRIVATE_KEY, PATH_TO_AMAZON_ROOT_CA_1, MESSAGE, TOPIC, and RANGE
+ENDPOINT = "a2xg0m2ynir6td-ats.iot.us-east-2.amazonaws.com"
+CLIENT_ID = "testDevicePriyeshLaptop"
+PATH_TO_CERTIFICATE = "./certificates/certificate.pem.crt" #add .crt
+PATH_TO_PRIVATE_KEY = "./certificates/private.pem.key"
+PATH_TO_AMAZON_ROOT_CA_1 = "./certificates/AmazonRootCA1.pem"
+MESSAGE = "Hello World from Priyesh"
+TOPIC = "priyesh/basic_simulator"
+
+# Spin up resources
+event_loop_group = io.EventLoopGroup(1)
+host_resolver = io.DefaultHostResolver(event_loop_group)
+client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+mqtt_connection = mqtt_connection_builder.mtls_from_path(
+            endpoint=ENDPOINT,
+            cert_filepath=PATH_TO_CERTIFICATE,
+            pri_key_filepath=PATH_TO_PRIVATE_KEY,
+            client_bootstrap=client_bootstrap,
+            ca_filepath=PATH_TO_AMAZON_ROOT_CA_1,
+            client_id=CLIENT_ID,
+            clean_session=False,
+            keep_alive_secs=6
+            )
+print("Connecting to {} with client ID '{}'...".format(
+        ENDPOINT, CLIENT_ID))
+# Make the connect() call
+connect_future = mqtt_connection.connect()
+# Future.result() waits until a result is available
+connect_future.result()
+print("Connected!")
+
+def publish_to_AWS(topic, payload):
+    mqtt_connection.publish(topic, json.dumps(payload), qos=mqtt.QoS.AT_LEAST_ONCE)
+    print("Published: '" + json.dumps(payload) + "' to the topic: " + topic)
+
+def disconnect_from_AWS():
+    disconnect_future = mqtt_connection.disconnect()
+    disconnect_future.result()
+
+while(1):
+    sim_data = {
+        "device_id" : "device_29129797",
+        "timestamp" : str(datetime.datetime.now()),
+        "temperature" : random.normalvariate(26, 1.5),
+        "humidity" : random.normalvariate(75, 3)
+    }
+
+    publish_to_AWS(TOPIC, sim_data)
+
+    time.sleep(5)
